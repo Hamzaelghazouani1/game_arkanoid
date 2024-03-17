6a68b75272f2afb0fffde6e8432d187174b79708
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-03-17 06:28:34 +0000

    feat : add params

--- script.js
+++ script.js
@@ -4,6 +4,16 @@
 const ball  = document.getElementById('ball');
 const paddle = document.getElementById('paddle');
 
+const level = document.getElementById('level');
+const score = document.getElementById('score');
+
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const reset = document.getElementById('reset');
+
+const gameOver = document.getElementById('gameOver');
+
+
 // Set the initial position of the ball :
 let directionX = Math.floor(Math.random() * 2)+1;
 let directionY = 1;
@@ -18,18 +28,53 @@ const DirectionY = {
     LEFT: 2
 }
 
+const Level = {
+    EASY: 1,
+    MEDIUM: 2,
+    HARD: 3
+}
+
+params = () => {
+    parseInt(score.innerText) == 10 ? level.innerText = Level.EASY : parseInt(score.innerText) == 20 ? level.innerText = Level.MEDIUM : parseInt(score.innerText) == 30 ? level.innerText = Level.HARD : '';
+    switch (parseInt(level.innerText)) {
+        case Level.EASY:
+            clearInterval(counter);
+            counter = setInterval(angelMove, 10);
+        break;
+        case Level.MEDIUM:
+            clearInterval(counter);
+            counter = setInterval(angelMove, 5);
+        break;
+        case Level.HARD:
+            clearInterval(counter);
+            counter = setInterval(angelMove, 1);
+        break;
+    }
+
+    if(parseInt(score.innerText) < 0){
+        clearInterval(counter);
+        gameOver.style.display = 'flex';
+        gameOver.style.opacity = 1;
+        document.removeEventListener('keydown', handlKey);
+    }
+}
+
 
 // Function to handle the ball movement :
 angelMove = () => {
     if(paddle.offsetLeft >= ball.offsetLeft-100 && paddle.offsetLeft < ball.offsetLeft && ball.offsetTop+10 >= 670){
         directionX = DirectionX.UP;
+        score.innerText++;
     }
+    params();
+
     switch (ball.offsetTop) {
         case 10:
             directionX = DirectionX.DOWN;
         break;
         case 690:
             directionX = DirectionX.UP;
+            score.innerText--;
         break;
     }
     switch (ball.offsetLeft) {
@@ -68,5 +113,30 @@ handleKeyPress = (event) => {
 }
 
 // Set the event listener for the Game :
-document.addEventListener('keydown', handleKeyPress);
+var handlKey = document.addEventListener('keydown', handleKeyPress);
 var counter = setInterval(angelMove, 10);
+
+
+// Set the event listener for the buttons :
+start.addEventListener('click', () => {
+    counter == undefined ? counter = setInterval(angelMove, 10) : null;
+});
+
+pause.addEventListener('click', () => {
+    clearInterval(counter);
+    counter = undefined;
+});
+
+reset.addEventListener('click', () => {
+    clearInterval(counter);
+    counter = undefined;
+    ball.style.top = '50%';
+    ball.style.left = '0%';
+    paddle.style.left = '50%';
+    gameOver.style.display = 'none';
+    gameOver.style.opacity = 0;
+    directionX = Math.floor(Math.random() * 2)+1;
+    directionY = 1;
+    score.innerText = 10;
+    level.innerText = Level.EASY;
+});
